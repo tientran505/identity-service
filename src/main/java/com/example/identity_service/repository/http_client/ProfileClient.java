4cbdaa07800944af4e0533d1b1483a9db8e58620
Tien <73010955+tientran505@users.noreply.github.com>
2025-02-18 23:37:41 +0700

    feat: add interceptor for profile ms

--- src/main/java/com/example/identity_service/repository/http_client/ProfileClient.java
+++ src/main/java/com/example/identity_service/repository/http_client/ProfileClient.java
@@ -1,13 +1,19 @@
 package com.example.identity_service.repository.http_client;
 
+import com.example.identity_service.configuration.AuthenticationRequestInterceptor;
 import com.example.identity_service.dto.request.ProfileCreationRequest;
 import com.example.identity_service.dto.response.UserProfileCreationResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "profile-service",  url = "${app.services.profile-service}")
+@FeignClient(
+        name = "profile-service",
+        url = "${app.services.profile-service}",
+        configuration = {AuthenticationRequestInterceptor.class}
+)
 public interface ProfileClient {
     @PostMapping(value = "/internal/profiles", produces = MediaType.APPLICATION_JSON_VALUE)
     UserProfileCreationResponse createProfile(@RequestBody ProfileCreationRequest profile);
